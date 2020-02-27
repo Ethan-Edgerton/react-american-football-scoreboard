770e6ef39fc2f1dd2f13c5c8be04e4f22c0d3d18
Ethan-Edgerton <Ethan.E.Edgerton@gmail.com>
2020-02-26 19:13:03 -0600

    MVP met with part stretch tasks

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  //use States
+  let [down, setDown] = useState(0)
+  let [quarter, setQuarter] = useState(0)
+
+  // Down Counter Function
+   function downCounter() {
+      if (down < 4) {
+        setDown( down + 1)
+      } else if (down === 4){
+        setDown(down - down)
+      }
+   }
+
+   //Quarter Counter Function
+   function quarterCounter() {
+    if (quarter < 4) {
+      setQuarter( quarter + 1)
+    } else if (quarter === 4){
+      setQuarter(quarter - quarter)
+    }
+ }
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{down}</div>
+        {/* Down Button */}
+          <button onClick = {downCounter}className="homeButtons__fieldGoal">Down</button> 
+
+
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
@@ -18,7 +43,9 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarter}</div>
+        {/* Quarter Button */}
+        <button onClick = {quarterCounter}className="homeButtons__fieldGoal">Quarter</button>
       </div>
     </div>
   );
